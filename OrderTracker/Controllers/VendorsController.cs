fd5bcb6c24e5c1621b58ffbec976571fb1a1316e
onurkaymak <onurkaymak@outlook.com>
2023-09-29 11:56:00 -0700

    add Vendors controller, add index route for VendorsController and add View for index route.

---
+++ OrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
