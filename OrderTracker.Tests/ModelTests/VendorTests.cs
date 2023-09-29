b6b9f15fe8612ddd541fc5b0f75b08e649ed721d
onurkaymak <onurkaymak@outlook.com>
2023-09-29 09:13:12 -0700

    pass test for Vendor class, create and get unique id.

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -36,5 +36,15 @@ namespace OrderTracker.Test
       string result = newVendor.Description;
       Assert.AreEqual(desc, result);
     }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Suzie's Cafe";
+      string desc = "Cute small cafe, long term customer.";
+      Vendor newVendor = new Vendor(name, desc);
+      int result = newVendor.Id;
+      Assert.AreEqual(0, result);
+    }
   }
 }
--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -4,13 +4,16 @@ namespace OrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Id { get; }
 
     public Vendor(string vendorName, string desc)
     {
       Name = vendorName;
       Description = desc;
+      Id = _instances.Count;
     }
   }
 }
