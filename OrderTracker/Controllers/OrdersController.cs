9de8f276973ad284a77fbde4fc0a81cd4a78c376
onurkaymak <onurkaymak@outlook.com>
2023-09-29 15:52:26 -0700

    add OrdersController controller and Show route show selected vendor orders.

---
+++ OrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Vendor selectedVendor = Vendor.Find(vendorId);
+      Order selectedOrder = Order.Find(orderId);
+
+      Dictionary<string, object> model = new Dictionary<string, object> { };
+
+      model.Add("vendor", selectedVendor);
+      model.Add("order", selectedOrder);
+      return View(model);
+    }
+  }
+}
